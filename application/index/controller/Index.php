1f9f775828e483db132e7aed9979d0a56373d64d
yangmingzhao <yangmingzhao@diag.launch>
2019-04-09 14:06:36 +0800

    windows

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -24,7 +24,7 @@ class Index extends Frontend
         $is_level = $this->user['gender'];
         //会员到期日期
         $birthday = $this->user['birthday'];
-        $createtime = $this->user['createtime'];
+        $deadline = $this->user['deadline'];
 
         $video_id = input('video_id',1);
         $video_list = db('channel')->where('name','not in','测试,TEST,test')->select();
@@ -35,7 +35,7 @@ class Index extends Frontend
         $current_video_url = $current_video['url'];
         $msg = '';
         //体验日期是否已经过
-        if($is_level == 0 && time() > strtotime('+5 days',$createtime))
+        if($is_level == 0 && time() > $deadline)
         {
             $msg = '尊敬的'.$this->auth->username.'用户，您的观看体验期已过，如需开通会员请添加客服微信：shan47636';
             $current_video_url = '';
@@ -46,6 +46,7 @@ class Index extends Frontend
             $msg = '尊敬的'.$this->auth->username.'会员，您购买的会员已过期，如需继续开通会员请添加客服微信：shan47636';
             $current_video_url = '';
         }
+
 
         $current_video_name = $current_video['name'];
         $this->assign('user_id',$this->auth->id);
