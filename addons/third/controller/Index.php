ac71289492b27664498132cd98c9139a0652ed12
yangmingzhao <yangmingzhao@diag.launch>
2019-04-09 20:57:26 +0800

    windows

--- addons/third/controller/Index.php
+++ addons/third/controller/Index.php
@@ -51,6 +51,7 @@ class Index extends Controller
         $platform = $this->request->param('platform');
         $share_id = $this->request->param('share_id');
 
+        Log::write('=======**********=========='.$share_id);
         if (!$this->app->{$platform}) {
             $this->error(__('Invalid parameters'));
         }
