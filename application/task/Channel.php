143beb8fbf087907215eadcee8ce21ceb966c3ed
yangmingzhao <yangmingzhao@diag.launch>
2019-04-12 10:09:26 +0800

    windows

--- application/task/Channel.php
+++ application/task/Channel.php
@@ -33,25 +33,20 @@ class Channel extends Command
             db('channel')->where('id','in',[186,193])->update(['url'=>$match[1]]);
         }
 
-        $cookie = Curl2::curl_request('http://wx.ottcom.cn/login/mlogin',['uname'=>'13713018282','upass'=>'aaaaaa'],0,1);
+        $cookie = Curl2::curl_request('http://wx.ottcom.cn/login/mlogin',['uname'=>'18928221189','upass'=>'111111'],0,1);
 
-        $video_ids = [];
+        //j2/j2备用/翡翠台（备用）/有线新闻
+        $video_ids = [184=>3590,185=>3344,183=>3574,200=>3586];
 
-        foreach ($video_ids as $video_id)
+        foreach ($video_ids as $key=>$video_id)
         {
             $video_play = Curl2::curl_request('http://wx.ottcom.cn/news/view/id/'.$video_id,[],$cookie['cookie'],0);
             preg_match('#<video[\s]+src="(.*?)"#im',$video_play,$match2);
             $video_url = $match2[1];
-            preg_match('#<h2[^>]+>(.*?)<\/h2>#is',$video_play,$match3);
-            $video_name = trim($match3[1]);
-            if(db('channel')->where(['name'=>$video_name])->find())
-            {
-                db('channel')->where(['name'=>$video_name])->update(['url'=>$video_url]);
-            }
-            else
-            {
-                db('channel')->insert(['name'=>$video_name,'url'=>$video_url]);
-            }
+            //preg_match('#<h2[^>]+>(.*?)<\/h2>#is',$video_play,$match3);
+            //$video_name = trim($match3[1]);
+            db('channel')->where(['id'=>$key])->update(['url'=>$video_url]);
+
         }
     }
     /*  protected function execute(Input $input, Output $output)
